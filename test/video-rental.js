118d76b6dbbdb63e1caf3ebef2f230b5a5321e35
Oleg <il.oleg@outlook.com>
2017-01-30 15:53:18 +0200

    create Video Rental

--- test/video-rental.js
+++ test/video-rental.js
@@ -1,3 +1,34 @@
-/**
+var assert = require('assert');
+var Barmen = require('../src/video');
+
+//## Видеопрокат
+//* Клиент может взять фильм в прокат
+//* Клиент может взять несколько фильмов и получить скидку 5% за каждый, но не более 15%
+//* Не выдавать больше 5 фильмов за раз одному клиенту
+//* Если клиент не вернул прошлый фильм, не выдавать ему новых
+//* Приведи друга, получи дополнительную скидку 10%
+//
+
+describe('Video Rental', function () {
+    it('The client can take in a movie rental', function () {
+
+    });
+
+    it('The client can take several movies and get a 5% discount for each, but no more than 15%', function () {
+
+    });
+
+    it('Do not give out more than 5 films for just one client', function () {
+
+    });
+
+    it('If the customer has not returned the last film, not to give him a new', function () {
+
+    });
+    it('Bring a friend, get an extra 10%', function () {
+
+    });
+
+});/**
  * Created by oleg on 30/01/2017.
  */
