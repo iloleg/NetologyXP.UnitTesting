cebd33a64e6be8b4293eaa2785a845048da7ddb1
Oleg <il.oleg@outlook.com>
2017-01-30 15:42:57 +0200

    create test bartender with the client

---
+++ test/Bar.js
@@ -0,0 +1,29 @@
+/**
+ * Created by oleg on 30/01/2017.
+ */
+//## Бар, бармен, посетители
+//* Клиент может попросить бармена налить виски
+//* Бармен откажет клиенту, если он заказывает уже 5й раз
+//* Клиент пьянеет, выпивая виски
+//* Бармен не наливает лицам младше 18 лет
+//* Бармен делает скидку 10%, если посетители пришли вдвоем и заказывают оба
+var assert = require('assert');
+var Barmen = require('../src/barmen');
+
+describe('bartender with the client', function () {
+    it('Pour whiskey if I ask and I got glass with 50 gram', function () {
+
+    });
+    it('pour whiskey - I ask 5 times - barmen rejects', function () {
+
+    });
+    it('pour whiskey - I drink and get drunk - barmen rejects', function () {
+
+    });
+    it('pour whiskey - I am less than 18 years old - barmen calls policeman', function () {
+
+    });
+    it('pour whiskey - I have discount if you order two- barmen make discount', function () {
+
+    });
+});
